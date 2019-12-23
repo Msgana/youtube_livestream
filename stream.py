eabee97d68c9e7d83531bb7e3122fdb8ea9f7857
Msgana <misganayohannes@gmail.com>
2019-12-23 16:35:08 -0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -11,7 +11,7 @@ print "startiing youtube livestream ....."
 print("Live streaming started!")
 
 # Video streaming to youtube 
-liveStreamCommand = 'raspivid -o - -t 0 -vf -hf -fps 20 -b 4500000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/u49s-rcva-k4t2-7u84'
+liveStreamCommand = 'raspivid -o - -t 0 -vf -hf -fps 20 -b 4500000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv youTubeUrl/youTubeStreamKey'
 
 #excute the live streaming command
 call([liveStreamCommand], shell=True)
